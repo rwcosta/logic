02980d073200b971478e5d145e06211e3a0bc233
Rennan Weslley <rennanweslley18@gmail.com>
2017-12-01 11:33:15 -0300

    Perfectionism ;)

--- Prolog/exercise.pl
+++ Prolog/exercise.pl
@@ -20,5 +20,5 @@ filho(X, Y) :- pais(X, Y, Mae); pais(X, Pai, Y).
 irmao(X, Y) :- pais(X, Pai, Mae), pais(Y, Pai, Mae).
 tio(X, Y)   :- (irmao(X, Pai), pais(Y, Pai, Cunhada)); (irmao(X, Mae), pais(Y, Cunhado, Mae)).
 avo(X, Y)   :- (filho(Pai, X), pais(Y, Pai, Nora)); (filho(Mae, X), pais(Y, Genro, Mae)).
-primo(X, Y) :- ( tio(Pai, X), pais(Y, Pai, Tia)); (tio(Mae, X), pais(Y, Tio, Mae)).
+primo(X, Y) :- (tio(Pai, X), pais(Y, Pai, Tia)); (tio(Mae, X), pais(Y, Tio, Mae)).
 
