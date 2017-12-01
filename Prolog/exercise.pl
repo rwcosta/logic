deaa80ae48975f20fa8e4de4191e5f8652fafe6d
Rennan Weslley <rennanweslley18@gmail.com>
2017-12-01 11:30:14 -0300

    First version of the exercise

---
+++ Prolog/exercise.pl
@@ -0,0 +1,24 @@
+homem(joao).
+homem(pedro).
+homem(carlos).
+
+homem(flavio).
+
+mulher(maria).
+mulher(julia).
+mulher(joyce).
+
+mulher(marta).
+
+pais(joao, pedro, maria).
+pais(julia, pedro, maria).
+
+pais(carlos, joao, marta).
+pais(joyce, flavio, julia).
+
+filho(X, Y) :- pais(X, Y, Mae); pais(X, Pai, Y).
+irmao(X, Y) :- pais(X, Pai, Mae), pais(Y, Pai, Mae).
+tio(X, Y)   :- (irmao(X, Pai), pais(Y, Pai, Cunhada)); (irmao(X, Mae), pais(Y, Cunhado, Mae)).
+avo(X, Y)   :- (filho(Pai, X), pais(Y, Pai, Nora)); (filho(Mae, X), pais(Y, Genro, Mae)).
+primo(X, Y) :- ( tio(Pai, X), pais(Y, Pai, Tia)); (tio(Mae, X), pais(Y, Tio, Mae)).
+
