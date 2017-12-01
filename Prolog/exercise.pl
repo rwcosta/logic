61f1134fe56e4ef1e7a37bfe1cc72226061a0e3d
Rennan Weslley <rennanweslley18@gmail.com>
2017-12-01 12:51:16 -0300

    Some changes

--- Prolog/exercise.pl
+++ Prolog/exercise.pl
@@ -1,14 +1,20 @@
 homem(joao).
 homem(pedro).
 homem(carlos).
+homem(carlosJr).
 
 homem(flavio).
+homem(cazuza).
 
 mulher(maria).
 mulher(julia).
 mulher(joyce).
+mulher(lisa).
+mulher(graça).
+mulher(paula).
 
 mulher(marta).
+mulher(luna).
 
 pais(joao, pedro, maria).
 pais(julia, pedro, maria).
@@ -16,9 +22,14 @@ pais(julia, pedro, maria).
 pais(carlos, joao, marta).
 pais(joyce, flavio, julia).
 
-filho(X, Y) :- pais(X, Y, Mae); pais(X, Pai, Y).
-irmao(X, Y) :- pais(X, Pai, Mae), pais(Y, Pai, Mae).
-tio(X, Y)   :- (irmao(X, Pai), pais(Y, Pai, Cunhada)); (irmao(X, Mae), pais(Y, Cunhado, Mae)).
-avo(X, Y)   :- (filho(Pai, X), pais(Y, Pai, Nora)); (filho(Mae, X), pais(Y, Genro, Mae)).
-primo(X, Y) :- (tio(Pai, X), pais(Y, Pai, Tia)); (tio(Mae, X), pais(Y, Tio, Mae)).
+pais(carlosJr, carlos, luna).
+pais(lisa, carlos, luna).
+pais(graça, cazuza, joyce).
+pais(paula, cazuza, joyce).
+
+filho(X, Y) :- pais(X, Y, _); pais(X, _, Y).
+irmao(X, Y) :- (X \== Y), pais(X, Pai, Mae), pais(Y, Pai, Mae).
+tio(X, Y)   :- irmao(X, Z), (pais(Y, Z, _); pais(Y, _, Z)).
+avo(X, Y)   :- filho(Z, X), (pais(Y, Z, _); pais(Y, _, Z)).
+primo(X, Y) :- (X \== Y), (tio(Z, X), (pais(Y, Z, _); pais(Y, _, Z))).
 
